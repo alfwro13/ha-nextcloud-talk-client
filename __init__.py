96dfb770fff0129e480e4c9c10a1dd16bcf920a7
Andre W <10945277+alfwro13@users.noreply.github.com>
2023-04-19 11:36:21 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,24 @@
-"""The Nextcloud Talk component."""
+"""The Nextcloud Talk Client integration."""
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+DOMAIN = "nextcloud_talk_client"
+
+
+async def async_setup(hass: HomeAssistant, config: dict):
+    """Set up the Nextcloud Talk Client integration."""
+    # Do nothing, as the integration does not support YAML configuration.
+
+    return True
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Set up the Nextcloud Talk Client entry."""
+    hass.async_add_job(hass.config_entries.async_forward_entry_setup(entry, "sensor"))
+
+    return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Unload the Nextcloud Talk Client entry."""
+    return await hass.config_entries.async_forward_entry_unload(entry, "sensor")
